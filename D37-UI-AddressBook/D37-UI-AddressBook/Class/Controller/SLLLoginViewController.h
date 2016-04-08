25272b7c3be60b9961d0a6ad5b47a833dcaa2a64
sunlei372 <121648522@qq.com>
2016-04-08 20:27:59 +0800

    1

---
+++ D37-UI-AddressBook/D37-UI-AddressBook/Class/Controller/SLLLoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  SLLLoginViewController.h
+//  D37-UI-AddressBook
+//
+//  Created by 孙磊 on 16/4/8.
+//  Copyright © 2016年 sunlei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SLLLoginViewController : UIViewController
+
+@end
---
+++ D37-UI-AddressBook/D37-UI-AddressBook/Class/Controller/SLLLoginViewController.m
@@ -0,0 +1,27 @@
+//
+//  SLLLoginViewController.m
+//  D37-UI-AddressBook
+//
+//  Created by 孙磊 on 16/4/8.
+//  Copyright © 2016年 sunlei. All rights reserved.
+//
+
+#import "SLLLoginViewController.h"
+
+@interface SLLLoginViewController ()
+
+@end
+
+@implementation SLLLoginViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
