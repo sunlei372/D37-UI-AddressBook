47815cbdf5b24df1d215187ac66e4fb392f6d7fb
sunlei372 <121648522@qq.com>
2016-04-08 19:47:18 +0800

    first

---
+++ D37-UI-AddressBook/D37-UI-AddressBook/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  D37-UI-AddressBook
+//
+//  Created by 孙磊 on 16/4/8.
+//  Copyright © 2016年 sunlei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
